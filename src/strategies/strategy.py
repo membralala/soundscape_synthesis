b25e489541610628c16580eef65e800e865a5ef4
philip.popien <philip.popien@tu-dortmund.de>
2025-02-03 12:05:46 +0100

    Simplify base class. Improve export for pathlib.Path

--- src/strategies/strategy.py
+++ src/strategies/strategy.py
@@ -1,13 +1,21 @@
+import pathlib
+
 import soundfile as sf
 
 
 class Strategy:
-    @property
-    def name(self):
-        return type(self).__name__
+    excludes = []
 
     def to_dict(self):
-        return dict(name=self.name, config=self.__dict__)
+        config = dict()
+        for key, value in self.__dict__.items():
+            if key in type(self).excludes:
+                continue
+            if isinstance(value, pathlib.Path):
+                config[key] = str(value)
+            else:
+                config[key] = value
+        return dict(name=type(self).__name__, config=config)
 
     def __call__(self, wavfile, outfile):
         raise NotImplementedError
